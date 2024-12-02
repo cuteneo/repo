a8009f1e589f92c87a546ad355ccca24cec93538
Anjali <151067805+cuteneo@users.noreply.github.com>
2024-12-02 16:41:12 +0530

    Create run_date_commands.cmd

---
+++ run_date_commands.cmd
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Go to the main directory (root directory)
+cd /
+
+# Create the folder in the main directory
+mkdir "Folder New Trigger"
+
+echo "Folder 'Folder with Azure pipeline' created successfully in the main directory!"
